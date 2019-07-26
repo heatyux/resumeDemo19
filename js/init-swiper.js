e2e9fce0e1f1ea7dc7387c0de9ab9209eb846199
heatyux <421743974@qq.com>
2019-07-26 11:52:19 +0800

    添加 MVC 的 View、Controller

--- js/init-swiper.js
+++ js/init-swiper.js
@@ -1,22 +1,28 @@
-! function() {
-  var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
+! function () {
+  var view = document.querySelector('#mySlides');
+  var controller = {
+    view: null,
+    swiper: null,
+    swiperOrtions: {
+      loop: true,
+      pagination: {
         el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
+      },
+      navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
+      },
+      scrollbar: {
         el: '.swiper-scrollbar',
+      },
     },
-  })
+    init: function (view) {
+      this.view = view,
+      this.initSwiper()
+    },
+    initSwiper: function () {
+      this.swiper = new Swiper(view.querySelector('.swiper-container'), this.swiperOrtions)
+    }
+  };
+  controller.init(view);
 }.call()
--- js/smoothly-navigation.js
+++ js/smoothly-navigation.js
@@ -1,82 +1,50 @@
-//二级菜单
 ! function () {
-
-  // let liTags = document.getElementsByClassName("menuTigger");
-  let liTags = document.querySelectorAll("nav.menu > ul > li")
-  for (let i = 0; i < liTags.length; i++) {
-    liTags[i].onmouseenter = function (x) {
-      x.currentTarget.classList.add("active");
-      // let li = x.currentTarget;
-      // let child = li.getElementsByTagName('ul')[0];
-      // let brother = a.nextSibling;
-      // while(brother.tagName !== "UL") {
-      //     brother = brother.nextSibling;
-      // }
-      // child.classList.add("active");
-    }
-    liTags[i].onmouseleave = function (x) {
-      x.currentTarget.classList.remove("active");
-      // let li = x.currentTarget;
-      // let child = li.getElementsByTagName('ul')[0];
-      // let brother = a.nextSibling;
-      // while(brother.tagName !== "UL") {
-      //     brother = brother.nextSibling;
-      // }
-      // child.classList.remove("active");
-    }
-  }
-
-  //滚动到指定元素
-  // let aTags = document.querySelectorAll("nav.menu > ul > li > a");
-  // for (let i = 0; i < aTags.length; i++) {
-  //     aTags[i].onclick = function (x) {
-  //         x.preventDefault();
-  //         let a = x.currentTarget;
-  //         let href = a.getAttribute("href");
-  //         let element = document.querySelector(href);
-  //         let top = element.offsetTop;
-  //         window.scrollTo(0, top - 80);
-  //     }
-  // }
-
-  //设置滚动时长与速度
-  let aTags = document.querySelectorAll('nav.menu > ul > li > a');
-
-  function animate(time) {
-    requestAnimationFrame(animate);
-    TWEEN.update(time);
-  }
-  requestAnimationFrame(animate);
-
-  for (let i = 0; i < aTags.length; i++) {
-    aTags[i].onclick = function (x) {
-      x.preventDefault();
-      let a = x.currentTarget;
-      let href = a.getAttribute("href");
-      let element = document.querySelector(href);
+  var view = document.querySelector('nav.menu');
+  var controller = {
+    view: null,
+    aTags: null,
+    initAnimation: function () {
+      function animate(time) {
+        requestAnimationFrame(animate);
+        TWEEN.update(time);
+      }
+      requestAnimationFrame(animate);
+    },
+    init: function (view) {
+      this.view = view;
+      this.initAnimation();
+      this.bindEvents();
+    },
+    scrollToElement: function(element) {
       let top = element.offsetTop;
-      // window.scrollTo(0, top - 80);
-
       let currentTop = window.scrollY;
       let targetTop = top - 80;
       let s = targetTop - currentTop;
       let t = Math.abs((s / 100) * 300);
-      if (t > 500) {
-        t = 500
-      }
+      if (t > 500) {t = 500}
 
-      const coords = {
-        y: currentTop
-      };
+      const coords = {y: currentTop};
       const tween = new TWEEN.Tween(coords)
-        .to({
-          y: targetTop
-        }, t)
+        .to({y: targetTop}, t)
         .easing(TWEEN.Easing.Cubic.InOut)
         .onUpdate(() => {
           window.scrollTo(0, coords.y);
         })
         .start();
-    }
+    },
+    bindEvents: function () {
+      //设置滚动时长与速度
+      let aTags = this.view.querySelectorAll('nav.menu > ul > li > a');
+      for (let i = 0; i < aTags.length; i++) {
+        aTags[i].onclick = (x) => {
+          x.preventDefault();
+          let a = x.currentTarget;
+          let href = a.getAttribute("href");
+          let element = document.querySelector(href);
+          this.scrollToElement(element);
+        }
+      }
+    },
   }
+  controller.init(view);
 }.call()
